b129cfcc3e0e3db7e53941db20368571587c7dbf
ushers956 <ushershaninz@gmail.com>
2024-09-24 18:09:02 +1200

    datatype

---
+++ datatype.py
@@ -0,0 +1,27 @@
+name = "usher"
+age = 12 
+is_student = True
+weight = 42.8
+
+# printing diffrent variable and there data type
+print("name :", name)
+print("data type of name is", type(name))
+
+print("age :", age)
+print("data type of age is", type(age))
+
+print("is_student :", is_student)
+print("data type of is_student is", type(is_student))
+
+print("weight :", weight)
+print("data type of weight is", type(weight))
+
+
+# type casting to convert the datatype of variables
+type("\n after type casting ....")
+age=str(age)
+print(age)
+print ("data type of age is", type(age))
+weight = int(weight)
+print("weight :", weight)
+print("data type of weight is", type(weight))
